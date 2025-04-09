e44ce059614eb8b7cf52e3608ff6ad37e0eb2a82
zhangfengcdt <feng@wherobots.com>
2025-04-09 16:27:26 -0700

    expose viewer port from docker

--- src/agent/vision/browser_viewer.js
+++ src/agent/vision/browser_viewer.js
@@ -4,5 +4,5 @@ const mineflayerViewer = prismarineViewer.mineflayer;
 
 export function addBrowserViewer(bot, count_id) {
     if (settings.show_bot_views)
-        mineflayerViewer(bot, { port: 3000+count_id, firstPerson: true, });
+        mineflayerViewer(bot, { host: '0.0.0.0', port: 3000+count_id, firstPerson: true, });
 }
